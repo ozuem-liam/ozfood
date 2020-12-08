2583dc42678b8f46d5713f9018bc2e2ea4fcce54
Williams Ozuem <ozuemdw@gmail.com>
2020-12-08 17:20:02 +0100

    Cut and paste all document inside the one root folder

---
+++ script/script.js
@@ -0,0 +1,55 @@
+const menuList = [
+    {
+        "name": "Taco(Goat)",
+        "price": "#2500"
+    },
+    {
+        "name": "Buritto(Goat)",
+        "price": "#2000"
+    },
+    {
+        "name": "Ensalada(Goat)",
+        "price": "#2500"
+    },
+    {
+        "name": "Taco(chicken)",
+        "price": "#2500"
+    },
+    {
+        "name": "Buritto(chicken)",
+        "price": "#2500"
+    },
+    {
+        "name": "Ensalada(chicken)",
+        "price": "#2500"
+    },
+    {
+        "name": "Taco(pork)",
+        "price": "#2500"
+    },
+    {
+        "name": "Buritto(pork)",
+        "price": "#2500"
+    },
+    {
+        "name": "Ensalada(pork)",
+        "price": "#2500"
+    },
+    {
+        "name": "Taco(fish)",
+        "price": "#2500"
+    },
+    {
+        "name": "lemonade",
+        "price": "#500"
+    },
+    {
+        "name": "chips",
+        "price": "#500"
+    }
+    
+]
+
+const menuChoice = function(yourChoice) {
+return 
+}
