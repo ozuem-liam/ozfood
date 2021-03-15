074520672d59ea3a96b1248b0d7e2087e638fca6
Williams Ozuem <ozuemdw@gmail.com>
2021-03-15 13:33:25 +0100

    hide postgres link

--- Server/app.js
+++ Server/app.js
@@ -11,22 +11,22 @@ const passport = require('passport');
 
 var pg = require('pg');
 
-var conString = 
-"postgres://lqvlwdho:vj4IEHr-qTEPqh4WNf9QiScr0rP8FGti@otto.db.elephantsql.com:5432/lqvlwdho" //Can be found in the Details page
-var client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT NOW() AS "theTime"', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log("Connected to database");
-    // >> output: 2018-08-23T14:02:57.117Z
-    // client.end();
-  });
-});
+// var conString = 
+// "" //Can be found in the Details page
+// var client = new pg.Client(conString);
+// client.connect(function(err) {
+//   if(err) {
+//     return console.error('could not connect to postgres', err);
+//   }
+//   client.query('SELECT NOW() AS "theTime"', function(err, result) {
+//     if(err) {
+//       return console.error('error running query', err);
+//     }
+//     console.log("Connected to database");
+//     // >> output: 2018-08-23T14:02:57.117Z
+//     // client.end();
+//   });
+// });
 
 dotenv.config();
 
